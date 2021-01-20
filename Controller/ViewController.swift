e1efbf5585cdfe469ec9b79e1ba00d821247b2e0
Ianis Richardier <ianis.richardier@gmail.com>
2021-01-20 14:21:50 +0100

    Fonctionnel avant test de refacto des contraintes

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -5,122 +5,141 @@
 //  Created by Richardier on 14/12/2020.
 //
 
+// TODO: police swipe left
+// Checker l'image pixelisée instagrid (assets, ratio...)
+// Faire en sorte que l'on puisse pas share tant que toutes les images sont pas remplies
+// remettre la grid vierge quand le share est terminé
+// Retravailler l'architecture du projet, savoir la décrire, citer et montrer le MVC du projet pour la soutenance (prendre screenshots)
+// Rename class "ViewController"
+// utiliser alertes
+
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
+    // MARK: - Outlet properties
+
     @IBOutlet weak var gridView: UIView!
     @IBOutlet var layoutButtons: [UIButton]!
     @IBOutlet var gridButtons: [UIButton]!
+    
+    
+    // MARK: - Simple properties
+    
     private var pictureButton: UIButton!
     
-    private func asImage(from view: UIView) -> UIImage {
-        UIGraphicsBeginImageContext(view.frame.size)
-        // sécuriser :
-        view.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return image
-    }
     
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         layoutButtons[2].isSelected = true
     }
-    
-    func hideGridButtons(topLeft: Bool, bottomRight: Bool) {
-        gridButtons[0].isHidden = topLeft
-        gridButtons[3].isHidden = bottomRight
-    }
-    
-    @IBAction func didTapLayoutButton(_ sender: UIButton) {
-        // Déselectionner tous les boutons
-        // Selectionner le bouton actuel
-        // Mettre à jour la main grid view avec le style correspondant au bouton séléctionné
-        for button in layoutButtons {
-            button.isSelected = false
-        }
-        sender.isSelected = true
-        
-        if sender == layoutButtons[0] {
-            hideGridButtons(topLeft: true, bottomRight: false)
-        } else if sender == layoutButtons[1] {
-            hideGridButtons(topLeft: false, bottomRight: true)
-        } else {
-            hideGridButtons(topLeft: false, bottomRight: false)
-        }
-    }
-    
-    @IBAction func didTapGridButton(_ sender: UIButton) {
-        pictureButton = sender
-        let imagePicker = UIImagePickerController()
-        // Quand touché, ouvre le menu pour séléctionner une image
-        // Choisir une image
-        // Adapter au bouton
-        // remplacer l'image "+" par l'image choisie
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.delegate = self
-        present(imagePicker, animated: true, completion: nil)
-    }
-    
-    func gridAnimation(x: CGFloat, y: CGFloat) {
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
-            self.gridView?.transform = CGAffineTransform(translationX: x, y: y)
-        })
-    }
-    
-    private func openShareController(sender: UIGestureRecognizer) {
-        if sender.state == .ended { // utiliser un switch sur le state
-            let image = asImage(from: gridView) // guard let pour sécuriser car asImage est optionnel
-            let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: [])
-            activityViewController.completionWithItemsHandler = { (nil, completed, _, error) in
-                if completed {
-                    self.gridAnimation(x: 0, y: 0)
-                } else {
-                    self.gridAnimation(x: 0, y: 0)
-                }
-            }
-            if let popoverController = activityViewController.popoverPresentationController {
-                popoverController.sourceView = self.view
-                popoverController.sourceRect = self.view.bounds
-            }
-            present(activityViewController, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func swipeGesture(_ sender: UISwipeGestureRecognizer) {
-        
-        switch sender.direction { // CGAffineTransform -> UIView.animate
-        case .up :
-            if UIDevice.current.orientation == UIDeviceOrientation.portrait {
-                self.gridAnimation(x: 0, y: -900)
-                openShareController(sender: sender)
-            }
-            
-        case .left :
-            if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
-                self.gridAnimation(x: -900, y: 0)
-                openShareController(sender: sender)
-            }
-        default : break
-        }
-    }
-    
-    func didSwipeToShare() {
-        // SI on a les cadres images remplies :
-        // Animation pour faire disparaitre grid view
-        // activer le share
-        // choisir l'objet de sharing
-        // remettre en ordre lorsque on a fini
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+   
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true) { [weak self] in
             if let image = info[.originalImage] as? UIImage {
                 self?.pictureButton?.setImage(image, for: .normal)
                 self?.pictureButton?.imageView?.contentMode = .scaleAspectFill
             }
         }
+    }
+    
+    private func gridViewAsImage(from view: UIView) -> UIImage? {
+        UIGraphicsBeginImageContext(view.frame.size)
+        // sécuriser :
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        view.layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    private func hideGridViewButtons(topLeft: Bool, bottomRight: Bool) {
+        gridButtons[0].isHidden = topLeft
+        gridButtons[3].isHidden = bottomRight
+    }
+    
+    private func gridViewAnimation(x: CGFloat, y: CGFloat) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.gridView?.transform = CGAffineTransform(translationX: x, y: y)
+        })
+    }
+    
+    
+    private func openShareController(sender: UIGestureRecognizer) {
+        if sender.state == .ended { // utiliser un switch sur le state
+            let image = gridViewAsImage(from: gridView) // guard let pour sécuriser car asImage est optionnel
+            let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: [])
+            activityViewController.completionWithItemsHandler = { (nil, completed, _, error) in
+                if completed {
+                    self.gridViewAnimation(x: 0, y: 0)
+                    // Nettoyer
+                } else {
+                    self.gridViewAnimation(x: 0, y: 0)
+                }
+            } // se débarasser de popover
+            if let popoverController = activityViewController.popoverPresentationController {
+                popoverController.sourceView = self.view
+                popoverController.sourceRect = self.view.bounds
+            }
+            present(activityViewController, animated: true)
+        }
+    }
+    
+    // MARK: - Action methods
+    
+    @IBAction func didTapLayoutButton(_ sender: UIButton) {
+        for button in layoutButtons {
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        
+        if sender == layoutButtons[0] {
+            hideGridViewButtons(topLeft: true, bottomRight: false)
+        } else if sender == layoutButtons[1] {
+            hideGridViewButtons(topLeft: false, bottomRight: true)
+        } else {
+            hideGridViewButtons(topLeft: false, bottomRight: false)
+        }
+    }
+    
+    @IBAction func didTapGridButton(_ sender: UIButton) {
+        pictureButton = sender // Changer le nom
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func swipeGesture(_ sender: UISwipeGestureRecognizer) {
+        
+        switch sender.direction {
+        case .up :
+            if UIDevice.current.orientation == UIDeviceOrientation.portrait {
+                self.gridViewAnimation(x: 0, y: -900)
+                openShareController(sender: sender)
+            }
+        case .left :
+            if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
+                self.gridViewAnimation(x: -900, y: 0)
+                openShareController(sender: sender)
+            }
+        default :
+            print("you swiped in the wrong direction")
+            break
+        }
+    }
+    
+    
+    
+    
+    
+    func didSwipeToShare() {
+        // Si on a les cadres images remplies :
+        // Animation pour faire disparaitre grid view
+        // activer le share
+        // choisir l'objet de sharing
+        // remettre en ordre lorsque on a fini
     }
 }
